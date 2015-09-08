2bb6bc9fc0b8a961ce7939e69759c68111c8e09d
BikeshDahal <bdahal2358@gmail.com>
2015-09-07 21:13:04 -0400

    Done

---
+++ reverse_each_word.rb
@@ -0,0 +1,9 @@
+def reverse_each_word(sentence)
+    array= sentence.split
+    count=0;
+    array.collect do |element|
+        array[count]=element.reverse
+        count+=1;
+    end
+    return array.join(" ")
+end
