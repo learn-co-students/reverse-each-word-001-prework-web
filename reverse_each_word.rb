c5944e9cba6407df78ace0c0e1307506e5a21d01
shmulim <shmuel.markel@gmail.com>
2015-11-19 13:15:43 -0800

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,9 @@
+# def reverse_each_word(str)
+#   complete = Array.new
+#   str.split.each { |el| complete << "#{el.reverse}" }
+#   complete.join(" ")
+# end
+
 def reverse_each_word(str)
-  complete = Array.new
-  str.split.each { |el| complete << "#{el.reverse}" }
-  complete.join(" ")
+  str.split.collect { |el| el.reverse}.join(" ")
 end
