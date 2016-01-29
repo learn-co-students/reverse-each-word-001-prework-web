3412b7d5922d61c157eca1e5b44e70c603cba9c4
aback1227 <aback1227@gmail.com>
2016-01-28 22:00:03 -0500

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -8,6 +8,14 @@ def reverse_each_word(string)
 end
 
 def reverse_each_word(string)
+  reversed = []
+  string.split.each do |word|
+     reversed << word.reverse
+  end
+  reversed.join(" ")
+end
+
+def reverse_each_word(string)
     string.split.collect {|word| word.reverse}.join(" ")   
 end
 
