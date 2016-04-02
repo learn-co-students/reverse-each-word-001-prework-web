c0b4c3e927af9cd456cf4a5dbda64f6eef8c7841
ToddRizley <todd.rizley@gmail.com>
2016-04-02 12:02:29 -0400

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,20 @@
+def reverse_each_word(string)
+  new_array = string.split(" ")
+  altered_array = []
+  new_array.each do |word|
+    altered_array << word.reverse!
+    altered_array.join(" ")
+  end
+  altered_array.join(" ")
+end
+
+
+##with collect
+
+def reverse_each_word(string)
+  new_array = string.split(" ")
+  new_array.collect do |word|
+    word.reverse!
+  end
+  new_array.join(" ")
+end
