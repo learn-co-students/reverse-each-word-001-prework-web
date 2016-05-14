38b9a66d2ff303f3430960992f0ada198ccefa24
Jonathan Alexander <jonathandalexander@gmail.com>
2016-05-14 10:51:03 -0400

    Done. Cleaned up code.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,18 +1,3 @@
 def reverse_each_word(string)
-
-#turn string into an array
-#reverse each individual string
-# in array place in new array
-#return new array
-
-myArray = string.split
-fArray = Array.new
-
-myArray.each do |x|
-fArray << x.reverse
-end
-
-string = fArray.join(" ")
-return string
-
+  string.split.collect { |word| word.reverse }.join(' ')
 end
