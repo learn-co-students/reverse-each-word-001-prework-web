5c0408bde28e69e2d72cf9905d73757fdcd6dd16
pkolodgy <pkolodgy@gmail.com>
2016-06-07 18:21:35 -0400

    solved reverse_each_word.rb using collect

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,9 @@
 def reverse_each_word(str)
-  new_arr = []
   arr = str.split(" ")
-  arr.each do |word|
-    new_arr.push(word.reverse)
-  end
-  return new_arr.join(" ")
+  new_arr = reverse(arr).join(" ")
+  return new_arr
+end
+
+def reverse(array)
+  array.collect {|word| word.reverse}
 end
