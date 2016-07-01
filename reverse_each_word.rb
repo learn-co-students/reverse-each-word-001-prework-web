4493c8209666dace370fa1240df2e05a4dad9bd0
Mohammed Omar <samiomarmohammed@gmail.com>
2016-07-01 19:16:09 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,12 @@
 def reverse_each_word(string)
+  new_array = string.split(" ")
+  new_array.map! do |word|
+    word.reverse
+  end
+  new_array.join(" ")
+end
+
+def reverse_each_word(string)
   new_array = []
   split_sentence = string.split(" ")
   split_sentence.each do |word|
