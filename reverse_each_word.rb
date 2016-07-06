bf9dc477e79bf9f0705bdcc25b07d4be193be26e
lgants <lgants@gmail.com>
2016-07-06 15:40:07 -0400

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,9 @@
-def reverse_each_word(sentence)
-  sentence.each do |word|
-    word.reverse!
-  end 
+def reverse_each_word(str)
+    new_sentence = []
+    words = str.split(" ")
+    words.each do |word|
+        new_sentence.push(word.reverse!)
+    end 
+    new_sentence = words.join(" ")
 end 
+
