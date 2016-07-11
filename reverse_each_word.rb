f78ca6d4011a6c79293ba3fa09149a6bf8236cb1
Robert J. Domingo <robert.j.domingo@gmail.com>
2016-07-11 19:29:52 -0400

    Use each and collect

---
+++ reverse_each_word.rb
@@ -0,0 +1,15 @@
+def reverse_each_word(string)
+	words = string.split
+	reversed_words = []
+	words.each do |word|
+		reversed_words.push(word.reverse)
+	end
+	reversed_words.join(" ")
+end
+
+def reverse_each_word(string)
+	words = string.split
+	words.collect do |word|
+		word.reverse
+	end
+end
