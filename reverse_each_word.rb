9a190fe3e4a6dd5150ab5278f316e23572ffc3cb
Dimitri Serov <dimitriserov@gmail.com>
2016-07-23 01:30:17 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,16 @@
+#.each method
+def reverse_each_word(sentence)
+  words = sentence.split(" ")
+  reversed_sentenece = []
+  words.each do |word|
+    reversed_sentenece << word.reverse!
+  end
+  reversed_sentenece.join(" ")
+end
+
+#.collect method
+def reverse_each_word(sentence)
+  words = sentence.split(" ")
+  words.collect {|word| word.reverse!}
+  words.join(" ")
+end
