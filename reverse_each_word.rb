05e7f6c83c5b6f39a8f937fdb91e3a8ea1a5f4e3
Mirim An <mimicatcodes@gmail.com>
2016-08-11 04:54:06 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,16 +1,62 @@
-def reverse_each_word(string_sentence)
-  arrayed_word = string_sentence.split(" ")
-  reversed = []
-  arrayed_word.each do |word|
-    reversed << word.reverse!
+#.each
+def reverse_each_word(string)
+  arrayed_string = string.split(" ")
+  reversed_string = []
+  arrayed_string.each do |word|
+    reversed_string << word.reverse
   end
-  reversed.join(" ")
+  reversed_string.join(" ")
 end
 
-def reverse_each_word(string_sentence)
-  arrayed_word = string_sentence.split(" ")
-  arrayed_word.collect do |word|
-    word.reverse!
+
+
+#.collect
+def reverse_each_word(string)
+  arrayed_string = string.split(" ")
+  reversed_string = arrayed_string.collect do |word|
+    word.reverse
   end
-  arrayed_word.join(" ")
+  reversed_string.join(" ")
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def reverse_each_word(string_sentence)
+#   arrayed_word = string_sentence.split(" ")
+#   reversed = []
+#   arrayed_word.each do |word|
+#     reversed << word.reverse!
+#   end
+#   reversed.join(" ")
+# end
+#
+# def reverse_each_word(string_sentence)
+#   arrayed_word = string_sentence.split(" ")
+#   arrayed_word.collect do |word|
+#     word.reverse!
+#   end
+#   arrayed_word.join(" ")
+# end
