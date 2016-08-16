c10fa435dee663c7d487d9b1d47d74a9f69a2e3c
Mirim An <mimicatcodes@gmail.com>
2016-08-16 22:54:04 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,20 +1,61 @@
-#.each
-def reverse_each_word(string)
-  arrayed_string = string.split(" ")
-  reversed_string = []
-  arrayed_string.each do |word|
-    reversed_string << word.reverse
+#each
+
+def reverse_each_word(sentence)
+  reversed = []
+  sentence.split.each do |w|
+    reversed << w.reverse
   end
-  reversed_string.join(" ")
+  reversed.join(" ")
 end
 
 
 
-#.collect
-def reverse_each_word(string)
-  arrayed_string = string.split(" ")
-  arrayed_string.collect do |word|
-    word.reverse!
+
+#collect
+def reverse_each_word(sentence)
+  reversed = sentence.split.collect do |w|
+    w.reverse
   end
-  arrayed_string.join(" ")
+  reversed.join(" ")
+
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# #.each
+# def reverse_each_word(string)
+#   arrayed_string = string.split(" ")
+#   reversed_string = []
+#   arrayed_string.each do |word|
+#     reversed_string << word.reverse
+#   end
+#   reversed_string.join(" ")
+# end
+#
+#
+#
+# #.collect
+# def reverse_each_word(string)
+#   arrayed_string = string.split(" ")
+#   arrayed_string.collect do |word|
+#     word.reverse!
+#   end
+#   arrayed_string.join(" ")
+# end
