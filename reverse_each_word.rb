73caf01f4bc3d604c13c77c200d4ff2d8f4f4dc9
Mirim An <mimicatcodes@gmail.com>
2016-08-16 22:54:26 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,4 @@
 #each
-
 def reverse_each_word(sentence)
   reversed = []
   sentence.split.each do |w|
@@ -8,54 +7,10 @@ def reverse_each_word(sentence)
   reversed.join(" ")
 end
 
-
-
-
 #collect
 def reverse_each_word(sentence)
   reversed = sentence.split.collect do |w|
     w.reverse
   end
   reversed.join(" ")
-
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# #.each
-# def reverse_each_word(string)
-#   arrayed_string = string.split(" ")
-#   reversed_string = []
-#   arrayed_string.each do |word|
-#     reversed_string << word.reverse
-#   end
-#   reversed_string.join(" ")
-# end
-#
-#
-#
-# #.collect
-# def reverse_each_word(string)
-#   arrayed_string = string.split(" ")
-#   arrayed_string.collect do |word|
-#     word.reverse!
-#   end
-#   arrayed_string.join(" ")
-# end
