88eef9ce3f017963a2337879cffef5e00516bdb2
Jason Kwong <jasonkwong11@gmail.com>
2017-09-03 19:52:10 -0700

    refactor

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,3 @@
 def reverse_each_word(sentence)
-  reversed_sentence_array = []
-  array = sentence.split(" ")
-  array.collect do |x|
-    reversed_sentence_array.push(x.reverse)
-  end
-  reversed_sentence_array.join(" ")
+  sentence.split(' ').collect {|word| word.reverse }.join(" ")
 end
